170cdf18b56545e75ddb02dc645e1122aba52835
Edgar Froes <edgar.froes@gmail.com>
2023-10-30 03:06:40 -0300

    Initial commit

---
+++ lib/domain/entities/country.dart
@@ -0,0 +1,11 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'country.freezed.dart';
+
+@freezed
+class Country with _$Country {
+  const factory Country({
+    required String id,
+    required String name,
+  }) = _Country;
+}
